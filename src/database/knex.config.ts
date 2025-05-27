ad0defb05b0fb41aaf2d93cfa5c66536750735d1
tobias-barakaa <ngairato@gmail.com>
2025-05-27 16:36:55 +0300

    database

--- src/database/knex.config.ts
+++ src/database/knex.config.ts
@@ -1,13 +1,11 @@
 import { Knex } from 'knex';
 
 const config: Knex.Config = {
-  client: 'postgres', // or 'postgresql', 'sqlite3'
+  client: 'pg',
   connection: {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '3306'),
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'moneysaver',
+    // connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL || '',
+    ssl: { rejectUnauthorized: false },
   },
   migrations: {
     directory: './src/database/migrations',
@@ -16,6 +14,35 @@ const config: Knex.Config = {
   seeds: {
     directory: './src/database/seeds',
   },
+  pool: {
+    min: 1,
+    max: 5, // Neon has connection limits, keep it lower
+  },
+  acquireConnectionTimeout: 60000,
 };
 
 export default config;
+
+
+
+// import { Knex } from 'knex';
+
+// const config: Knex.Config = {
+//   client: 'postgres', // or 'postgresql', 'sqlite3'
+//   connection: {
+//     host: process.env.DB_HOST || 'localhost',
+//     port: parseInt(process.env.DB_PORT || '3306'),
+//     user: process.env.DB_USER || 'root',
+//     password: process.env.DB_PASSWORD || '',
+//     database: process.env.DB_NAME || 'moneysaver',
+//   },
+//   migrations: {
+//     directory: './src/database/migrations',
+//     extension: 'ts',
+//   },
+//   seeds: {
+//     directory: './src/database/seeds',
+//   },
+// };
+
+// export default config;
