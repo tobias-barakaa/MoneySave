41be2afd9f08a62086047552e6a1963965eb9ce2
tobias-barakaa <ngairato@gmail.com>
2025-05-28 18:42:30 +0300

    extractRequestFromHeader

--- src/auth/guards/access-token/access-token.guard.ts
+++ src/auth/guards/access-token/access-token.guard.ts
@@ -1,11 +1,36 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { Request } from 'express';
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
+  constructor(
+    /**
+     * Inject jwtService
+     */
+    private readonly jwtService: JwtService,
+
+    /**
+     * Inject jwtConfiguration
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1]; // Assuming Bearer token format
+    // Extract the request from the execution context 
+    // extract the token from the header
+    // validate the token
     return true;
+  }
+
+  private extractRequestFromHeader(request: Request) {
+    const [_, token] = request.headers.authorization?.split(' ') || [];
   }
 }
